d5131373a0e088610dc059d9e24eebd788c029f3
Nick Kean <109487676+NJK-NMIT@users.noreply.github.com>
2022-10-06 20:41:08 +1300

    Full MVC split

---
+++ controller/Controller_dm.py
@@ -0,0 +1,9 @@
+"""
+This is where the UI interaction lives
+
+"""
+
+import os.path
+from model.Model_dm import Model_dm
+
+
---
+++ model/load_remote_excel.py
@@ -0,0 +1,20 @@
+"""
+Deals with grabbing files from a ARLA website
+
+
+"""
+
+
+def load_remote_excel():
+    """
+    Pulls the excel file from the ARLA website and loads it.
+
+    NOT YET IMPLEMENTED
+
+    Args:
+        None.  The application knows where to find the ARLA data
+    Returns:
+        string: the URL of the excel file
+    """
+    pass
+
