5a7e8416d55eb8ceb5d1ffe1393b350cf97e6e70
Nick Kean <109487676+NJK-NMIT@users.noreply.github.com>
2022-11-06 12:09:19 +1300

    MD5 the passwords.  A little bit more chat stuff.

---
+++ controller/chat.py
@@ -0,0 +1,22 @@
+"""
+Chat stuff
+
+"""
+
+
+def send(*args, **kwargs):
+    """
+    Send a message to the chat system
+
+    Args:
+        string: Something
+        string: Something
+    Returns:
+        True/False: Was the send successfull?
+    """
+
+
+    if True:
+        return True
+    else:
+        return False
--- model/access.py
+++ model/access.py
@@ -1,12 +1,17 @@
 """
-Access to the application is controlled from here
+Access to the application is controlled from here.
+Passwords in the DB are MD5 encoded
 
 The only valid user/pass combos are
-    "bob" / "x"
-    "nick" / "x"
+    "bob" / "b"
+    "nick" / "z"
     "todd" / "x"
 """
 
+import hashlib
+#import model.network.jsn_drop_service as json
+import requests
+import json
 
 def is_password_valid(login, password):
     """
@@ -18,11 +23,13 @@ def is_password_valid(login, password):
     Returns:
         True/False: Is the given password the right one for the given login name
     """
-    if login == "bob" and password == "x":
+    md5pass = hashlib.md5(password.encode('utf-8')).hexdigest()
+
+    if login == "bob" and md5pass == "92eb5ffee6ae2fec3ad71c777531578f":
         return True
-    elif login == "nick" and password == "x":
+    elif login == "nick" and md5pass == "fbade9e36a3f36d3d676c1b808451dd7":
         return True
-    elif login == "todd" and password == "x":
+    elif login == "todd" and md5pass == "9dd4e461268c8034f5c8564e155c67a6":
         return True
     else:
         return False
