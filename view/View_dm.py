59feb254d36ed03ae3657c2e7bb261f60dfd9896
Nick Kean <109487676+NJK-NMIT@users.noreply.github.com>
2022-11-18 15:24:06 +1300

    Input chatbox shouhld be big, not the submit button!

--- view/View_dm.py
+++ view/View_dm.py
@@ -72,8 +72,8 @@ def make_the_window(dm: Model_dm):
     # The chat interface
     chatbox   = [ sg.Multiline('Empty chat', size=(80,4), font='Any 12',
                   key='-CHATTEXT-', background_color='lightgrey', autoscroll=True ) ]
-    chatinput = [ sg.Input('Say something ...',  size=(60, 1), do_not_clear=False, key="-CHATSEND-"),
-                  sg.Button('Submit', visible=True, font='Any 16', bind_return_key=True) ]
+    chatinput = [ sg.Input('Say something ...',  size=(60, 1), do_not_clear=False, key="-CHATSEND-", font='Any 16'),
+                  sg.Button('Submit', visible=True, font='Any 12', bind_return_key=True) ]
     controller.Controller_dm.add_control('Submit', controller.chat.send)
 
     # Focus and Zoom stuff
